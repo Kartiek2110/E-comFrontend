897274e8b52d863383f09a5f0c7a39aff4d0633e
Kartieky Bhardwaj <bhardwaj93kartieky@gmail.com>
2024-10-22 12:19:41 +0530

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Home from './Components/home'
+import Home from './Components/Home'
 import Signup from './Components/signup'
 
 
